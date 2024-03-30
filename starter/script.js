4fee22e1884995c25974ffee5e549f1a12de84ac
auriliu <149513433+auriliu@users.noreply.github.com>
2024-03-30 19:43:23 +0100

    BMI challenge added

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,52 @@
-let js = "amazing";
-if (js === "amazing") alert("javascript can be fun!");
+// let js = "amazing";
+// if (js === "amazing") alert("javascript can be fun!");
 
-console.log(2 + 2);
+// console.log(2 + 2);
+// console.log(typeof 3);
+
+// let year;
+// console.log(year); //undefined
+
+// let x = 15;
+// x += 10; //25
+// console.log(x);
+
+// x *= 4; //100
+// console.log(x);
+
+// x++; //101
+// console.log(x);
+
+// BMI calculator
+
+// const BMI = mass / height * height
+
+// data 1
+const massMark = 78;
+const heightMark = 1.69;
+
+const markBMI = massMark / (heightMark * heightMark);
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const johnBMI = massJohn / (heightJohn * heightJohn);
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
+
+// data 2
+const massMark2 = 95;
+const heightMark2 = 1.88;
+
+const markBMI2 = massMark2 / (heightMark2 * heightMark2);
+
+const massJohn2 = 85;
+const heightJohn2 = 1.76;
+
+const johnBMI2 = massJohn2 / (heightJohn2 * heightJohn2);
+
+const markHigherBMI2 = markBMI2 > johnBMI2;
+
+console.log(markHigherBMI2);
