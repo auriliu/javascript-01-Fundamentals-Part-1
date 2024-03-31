5fb3e45cd5c9f334656bd4d7e4588970f8090127
auriliu <149513433+auriliu@users.noreply.github.com>
2024-03-31 20:46:51 +0200

    temple literals

--- starter/script.js
+++ starter/script.js
@@ -17,36 +17,62 @@
 // x++; //101
 // console.log(x);
 
-// BMI calculator
+// BMI calculator //////////////////////////////////////////////
+// BMI calculator //////////////////////////////////////////////
+// BMI calculator //////////////////////////////////////////////
 
 // const BMI = mass / height * height
 
 // data 1
-const massMark = 78;
-const heightMark = 1.69;
+// const massMark = 78;
+// const heightMark = 1.69;
 
-const markBMI = massMark / (heightMark * heightMark);
+// const markBMI = massMark / (heightMark * heightMark);
 
-const massJohn = 92;
-const heightJohn = 1.95;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-const johnBMI = massJohn / (heightJohn * heightJohn);
+// const johnBMI = massJohn / (heightJohn * heightJohn);
 
-const markHigherBMI = markBMI > johnBMI;
+// const markHigherBMI = markBMI > johnBMI;
 
-console.log(markHigherBMI);
+// console.log(markHigherBMI);
 
 // data 2
-const massMark2 = 95;
-const heightMark2 = 1.88;
+// const massMark2 = 95;
+// const heightMark2 = 1.88;
 
-const markBMI2 = massMark2 / (heightMark2 * heightMark2);
+// const markBMI2 = massMark2 / (heightMark2 * heightMark2);
 
-const massJohn2 = 85;
-const heightJohn2 = 1.76;
+// const massJohn2 = 85;
+// const heightJohn2 = 1.76;
 
-const johnBMI2 = massJohn2 / (heightJohn2 * heightJohn2);
+// const johnBMI2 = massJohn2 / (heightJohn2 * heightJohn2);
 
-const markHigherBMI2 = markBMI2 > johnBMI2;
+// const markHigherBMI2 = markBMI2 > johnBMI2;
 
-console.log(markHigherBMI2);
+// console.log(markHigherBMI2);
+
+// end of BMI calculator //////////////////////////////////////////////
+// end of BMI calculator //////////////////////////////////////////////
+// end of BMI calculator //////////////////////////////////////////////
+
+// template literals:
+
+const firstName = "marcus";
+const job = "coder";
+const birthYear = 1991;
+
+const marcus = `hi there! my name is ${firstName} and I'm a ${job} and I've been coding since ${birthYear} `;
+
+console.log(marcus);
+
+console.log(
+  "string with \n\
+multiple \n\
+lines"
+);
+
+console.log(`string with
+multiple
+lines`);
